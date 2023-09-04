cdb2533fb2d229ca7576c9543e88e596e6a492c9
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-09-04 16:49:16 +0500

    feat: product app's admin model registering and serializer basic usages astoblished

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Banner, Product, ProductBrand, ProductCategory,
+                     ProductImage, ProductTag, ProductType, Section, Story,
+                     StoryContent, Volume)
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("file", "url")
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+
+
+@admin.register(ProductBrand)
+class ProductBrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(ProductTag)
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+    list_display = ("size", "type")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "currency", "brand")
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ("image", "product")
+
+
+@admin.register(StoryContent)
+class StoryContentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
