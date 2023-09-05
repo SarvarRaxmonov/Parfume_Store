f3c209a71a468ebc900bb3380afbee963953d95e
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-09-05 15:44:20 +0500

    feat:new migration file created

--- apps/cart/models/orders.py
+++ apps/cart/models/orders.py
@@ -13,6 +13,13 @@ class Liked(BaseModel):
     product = models.ForeignKey(Product, related_name="liked_product", on_delete=models.CASCADE)
     is_saved = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Liked'
+        verbose_name_plural = 'Liked'
+
 
 class Order(BaseModel):
     class OrderTextChoice(models.TextChoices):
@@ -25,9 +32,23 @@ class Order(BaseModel):
     number = models.IntegerField(unique=True)
     cashback = models.IntegerField()
 
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
 
 class Review(BaseModel):
     user = models.ForeignKey(User, related_name="user_reviews", on_delete=models.CASCADE)
     order = models.ForeignKey(Order, related_name="order_reviews", on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     msg = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
