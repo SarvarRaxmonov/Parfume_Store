37bc73abdbfd0c7f84e24af877a3db73f85280ff
abduraximov <bugatti2013@bk.ru>
2023-09-07 10:52:36 +0500

    feat:create notification bug fix

--- apps/user/signals.py
+++ apps/user/signals.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 
 from apps.user.models import Profile, User
 
+# from apps.user.models.notifications import Notification, ReadNotification
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -13,3 +15,13 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=Profile)
 def delete_user_when_deleted_profile(sender, instance, **kwargs):
     instance.user.delete()
+
+
+#
+# @receiver(post_save, sender=Notification)
+# def create_read_notifications(sender, instance, created, **kwargs):
+#     if created and instance.to_all:
+#         non_staff_user = User.objects.filter(is_staff=False)
+#
+#         for user in non_staff_user:
+#             ReadNotification.objects.create(user=user, notification=instance, is_read=False)
